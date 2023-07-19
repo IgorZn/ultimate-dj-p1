9088341ff709136d1b5ed29c364da2ca0b88d791
igor.znamensky <igor.znamensky@gmail.com>
2023-07-19 11:00:42 +0300

    41-50 (ORM), next: 41

--- UltimateDjP1/settings.py
+++ UltimateDjP1/settings.py
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'UltimateDjP1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': 'AdminGuru',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
