0ab9225dc620b06b1b0b0d3f3221ff4cd5eba3bd
igor.znamensky <igor.znamensky@gmail.com>
2023-07-26 10:25:50 +0300

    41-50 (ORM), next: 48

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q, F
+
+from store.models import Customer, Product
 
 # Create your views here.
 
 
 def say_hello(request):
+    customers = None
+    products = Product.objects.all().filter(
+        Q(inventory__gt=20) & Q(unit_price__lt=20)
+    ).order_by('unit_price')
+
+    products = Product.objects.all().filter(
+        inventory=F('collections_id')
+    ).order_by('unit_price')
+
+    products = Product.objects.all()[5:10]
+
+    products = Product.objects.values('id', 'title', 'collections__title')[:10]
+    products = Product.objects.values_list('id', 'title', 'collections__title')[:10]
+
     context = {
-        'name': 'Igor'
+        'name': 'Igor',
+        'customers': customers,
+        'products': products
     }
     return render(request, 'hello.html', context)
