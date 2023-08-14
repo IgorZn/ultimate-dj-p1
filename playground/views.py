e4d06b61034d79eac451c2d34e3156e0b5b9d389
IgorZn <igor.znamensky@gmail.com>
2023-08-14 13:50:34 +0300

    next: 54

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.db.models import Q, F
+from django.db.models import Q, F, Value, Func
+from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Max, Sum, Avg, Min
-from django.db.models import Value
 
 from store.models import Customer, Product
 
@@ -57,6 +57,14 @@ def say_hello(request):
 
     queryset = Customer.objects.annotate(is_new_old=Value(True))
     queryset = Customer.objects.annotate(new_id=F('id') * 2)
+
+    # call DB function CONCAT
+    queryset = Customer.objects.annotate(
+        full_name=Func(
+            F('first_name'), Value(' '), F('last_name'), function='CONCAT' ))
+
+    queryset = Customer.objects.annotate(
+        full_name=Concat('first_name', Value(' '), 'last_name'))
 
     context = {
         'name': 'Igor',
