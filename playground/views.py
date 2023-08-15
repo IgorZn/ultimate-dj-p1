c17b5c88ef5eb709940327b20003a992555f3a24
IgorZn <igor.znamensky@gmail.com>
2023-08-15 07:56:49 +0300

    next: 56

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Q, F, Value, Func
+from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Max, Sum, Avg, Min
 
@@ -69,6 +69,11 @@ def say_hello(request):
     # Grouping
     queryset = Customer.objects.annotate(
         order_count=Count('order'))
+
+    # ExpressionWrapper
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
+    queryset = Product.objects.annotate(
+        discounted_price=discounted_price)
 
     context = {
         'name': 'Igor',
