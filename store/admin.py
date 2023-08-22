6b97c9ac63e7aa04587068bd808a7338d9a1b205
IgorZn <igor.znamensky@gmail.com>
2023-08-22 10:00:20 +0300

    next: 68

--- store/admin.py
+++ store/admin.py
@@ -10,4 +10,15 @@ class AdminCollection(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class AdminProduct(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'description', 'unit_price']
+    list_display_links = ['description']
+    list_editable = ['title', 'unit_price']
+    list_per_page = 25
+
+
+@admin.register(models.Customer)
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['first_name', 'email', 'phone', 'membership']
+    list_display_links = ['first_name']
+    list_editable = ['membership', 'email']
+    list_per_page = 10
