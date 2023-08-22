282cb5b6b889a3cbec8a62b29d9d758a2710a21a
IgorZn <igor.znamensky@gmail.com>
2023-08-22 17:32:09 +0300

    next: 70

--- store/admin.py
+++ store/admin.py
@@ -10,10 +10,20 @@ class AdminCollection(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class AdminProduct(admin.ModelAdmin):
-    list_display = ['title', 'description', 'unit_price', 'inventory_status']
+    list_display = [
+        'title',
+        'description',
+        'unit_price',
+        'inventory_status',
+        'collections_title'
+    ]
     list_display_links = ['description']
     list_editable = ['title', 'unit_price']
     list_per_page = 25
+    list_select_related = ['collections']
+
+    def collections_title(self, product):
+        return product.collections.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -27,4 +37,15 @@ class AdminCustomer(admin.ModelAdmin):
     list_display = ['first_name', 'email', 'phone', 'membership']
     list_display_links = ['first_name']
     list_editable = ['membership', 'email']
+    list_per_page = 10
+
+
+@admin.register(models.Order)
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer_title']
+    list_select_related = ['customer']
+
+    def customer_title(self, order):
+        return f'{order.customer.first_name} {order.customer.last_name}'
+
     list_per_page = 10
